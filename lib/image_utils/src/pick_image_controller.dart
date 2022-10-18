0e8ddd8ed76f361a191c754ec0062faafccf65b8
Ahmed Ragab <ahmed_ragab20006@yahoo.com>
2022-10-18 06:56:09 +0200

    Update

--- lib/image_utils/src/pick_image_controller.dart
+++ lib/image_utils/src/pick_image_controller.dart
@@ -12,9 +12,9 @@ import 'package:super_widgets/super_widgets.dart';
 class PickImageController {
   static PickImageController get instance => PickImageController();
 
-  Future<String?> pickDirectory(SuperImageClass imageClass, {String dialogTitle='Pick a directory',bool lockParentWindow=true,String? initialDirectory}) async {
+  Future<String> pickDirectory(SuperImageClass imageClass, {String dialogTitle='Pick a directory',bool lockParentWindow=true,String? initialDirectory}) async {
     try {
-      return await FilePicker.platform.getDirectoryPath(dialogTitle: dialogTitle,lockParentWindow: lockParentWindow,initialDirectory: initialDirectory);
+      return await FilePicker.platform.getDirectoryPath(dialogTitle: dialogTitle,lockParentWindow: lockParentWindow,initialDirectory: initialDirectory)??'';
     } on Exception catch (e) {
       mPrintError('pickDirectory Exception $e');
       return '';
