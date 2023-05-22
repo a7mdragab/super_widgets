8e8155bea4badcf3a343603219fa697de74e8e4e
a7mdragab <ahmed_ragab20006@yahoo.com>
2023-05-22 10:59:19 +0300

    Updates

--- lib/widgets/src/super_datefield.dart
+++ lib/widgets/src/super_datefield.dart
@@ -1,4 +1,5 @@
 import 'package:dart_extensions/dart_extensions.dart';
+import 'package:date_field/date_field.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
@@ -37,10 +38,11 @@ class _SuperDateFieldState extends State<SuperDateField> {
 
   @override
   Widget build(BuildContext context) {
-    return DateTimeField(
+    return DateTimeFormField(
       enabled: widget.enabled,
-      format: DateFormat(widget.dateFormat, LanguageService.to.getLocale.languageCode),
-      decoration: const InputDecoration().applyDefaults(context.theme.inputDecorationTheme).copyWith(
+
+      //region Decorator
+      decoration: const InputDecoration().applyDefaults(Get.context!.theme.inputDecorationTheme).copyWith(
             isDense: true,
             isCollapsed: true,
             alignLabelWithHint: true,
@@ -63,22 +65,61 @@ class _SuperDateFieldState extends State<SuperDateField> {
             hintText: widget.hint,
             hintStyle: const TextStyle(fontSize: 12, color: Colors.grey),
           ),
-      initialValue: widget.initialDate,
-      controller: mController,
-      validator: FormBuilderValidators.compose(widget.validators!),
-      onShowPicker: (BuildContext context, DateTime? currentValue) async {
-        var res = await showDatePicker(
-          context: context,
-          firstDate: widget.firstDate ?? DateTime(1900),
-          lastDate: widget.lastDate ?? DateTime(2050),
-          initialDate: currentValue ?? widget.initialDate ?? DateTime.now(),
-        );
-        mController.text = res == null ? '' : res.toDateStr(widget.dateFormat);
-        // mController.text = res == null ? '' : 'res.toDateStr(widget.dateFormat)';
+      //endregion Decorator
 
-        setState(() {});
-        return res;
-      },
+      dateFormat: DateFormat(widget.dateFormat, LanguageService.to.getLocale.languageCode),
+      initialValue: widget.initialDate,
+      initialDate: widget.initialDate,
+      firstDate: widget.firstDate,
+      lastDate: widget.lastDate,
+      mode: DateTimeFieldPickerMode.date,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      // validator: (e) => (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+      validator: FormBuilderValidators.compose(widget.validators!),
+      onDateSelected: widget.onChanged,
     );
+    // return DateTimeField(
+    //   enabled: widget.enabled,
+    //   format: DateFormat(widget.dateFormat, LanguageService.to.getLocale.languageCode),
+    //   decoration: const InputDecoration().applyDefaults(context.theme.inputDecorationTheme).copyWith(
+    //         isDense: true,
+    //         isCollapsed: true,
+    //         alignLabelWithHint: true,
+    //         contentPadding: const EdgeInsets.all(16),
+    //         floatingLabelBehavior: FloatingLabelBehavior.always,
+    //         filled: true,
+    //         fillColor: Colors.white,
+    //         suffixIcon: mController.text.isNullOrWhiteSpace || !widget.enabled
+    //             ? null
+    //             : IconButton(
+    //                 onPressed: () {
+    //                   mController.clear();
+    //                   setState(() {});
+    //                 },
+    //                 icon: const Icon(Icons.close),
+    //               ),
+    //         suffixIconConstraints: const BoxConstraints(maxWidth: 40),
+    //         labelStyle: TextStyle(fontSize: 16, color: context.theme.primaryColor),
+    //         labelText: widget.hint,
+    //         hintText: widget.hint,
+    //         hintStyle: const TextStyle(fontSize: 12, color: Colors.grey),
+    //       ),
+    //   initialValue: widget.initialDate,
+    //   controller: mController,
+    //   validator: FormBuilderValidators.compose(widget.validators!),
+    //   onShowPicker: (BuildContext context, DateTime? currentValue) async {
+    //     var res = await showDatePicker(
+    //       context: context,
+    //       firstDate: widget.firstDate ?? DateTime(1900),
+    //       lastDate: widget.lastDate ?? DateTime(2050),
+    //       initialDate: currentValue ?? widget.initialDate ?? DateTime.now(),
+    //     );
+    //     mController.text = res == null ? (currentValue ?? widget.initialDate)!.toDateStr(widget.dateFormat) : res.toDateStr(widget.dateFormat);
+    //     // mController.text = res == null ? '' : 'res.toDateStr(widget.dateFormat)';
+    //
+    //     setState(() {});
+    //     return res;
+    //   },
+    // );
   }
 }
